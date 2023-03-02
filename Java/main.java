94b6375926c4f6bfc2b0dcda05dc52fd091dfdb4
MavelSterling <mavelynsl@gmail.com>
2023-03-02 02:38:43 -0500

    metodo de imprimir car

--- Java/main.java
+++ Java/main.java
@@ -7,12 +7,14 @@ class Main {
         car.license =   "AMQ123";
         car.driver= "Android";
         car.passegenger = 4;
-        System.out.println("Car License: " + car.license);
+        //System.out.println("Car License: " + car.license);
+        car.printDataCar();
 
         Car car2 = new Car();
         car2.license = "QWE567";
         car2.driver = "Apple";
         car2.passegenger = 3;
-        System.out.println("Car License: " + car2.license);
+        //System.out.println("Car License: " + car2.license);
+        car2.printDataCar();
     }
 }
