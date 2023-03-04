69a16a7a4151b49ccf6e3ef808dd03e9e5ca7c0e
MavelSterling <mavelynsl@gmail.com>
2023-03-03 20:33:17 -0500

    Update main.java

--- Java/main.java
+++ Java/main.java
@@ -3,16 +3,16 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.license =   "AMQ123";
-        car.driver= "Android";
+        Car car = new Car("AMQ123", new Account("Android", "AND123"));
+        //car.license =   "AMQ123";
+        //car.driver= "Android";
         car.passegenger = 4;
         //System.out.println("Car License: " + car.license);
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver = "Apple";
+        Car car2 = new Car("QWE567", new Account("Apple", "AND876"));
+        //car2.license = "QWE567";
+        //car2.driver = "Apple";
         car2.passegenger = 3;
         //System.out.println("Car License: " + car2.license);
         car2.printDataCar();
