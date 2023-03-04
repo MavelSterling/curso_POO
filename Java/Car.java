714e80035a0829e40b8f7452f291bf07bb3003fd
MavelSterling <mavelynsl@gmail.com>
2023-03-03 20:28:06 -0500

    Update Car.java

--- Java/Car.java
+++ Java/Car.java
@@ -3,8 +3,13 @@ package Java;
 class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passegenger;
+
+    public Car(String license, Account driver){
+        this.license=license;
+        this.driver=driver;
+    }
 
     void printDataCar(){
         System.out.println("License: " + license + " Driver: " + driver);
