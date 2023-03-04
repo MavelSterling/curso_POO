7299881b5acd797633118ec7dec094059dcc3ad6
MavelSterling <mavelynsl@gmail.com>
2023-03-03 21:56:58 -0500

    Se agrega el método constructor python

--- python/main.py
+++ python/main.py
@@ -1,13 +1,16 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car= Car()
-    car.license = "AMS234"
-    car.driver = "Android"
+    car= Car("AMS234", Account("Apple","AND876"))
     print(vars(car))
+    print(vars(car.driver))
+    #car.license = "AMS234"
+    #car.driver = "Android"
+    #print(vars(car))
     
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Apple"
-    print(vars(car2))
+    #car2 = Car()
+    #car2.license = "QWE567"
+    #car2.driver = "Apple"
+    #print(vars(car2))
