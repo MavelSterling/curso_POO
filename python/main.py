36af3b859d2c15d26c58629e8fed534c855cd2b7
MavelSterling <mavelynsl@gmail.com>
2023-03-05 01:32:46 -0500

    herencia en python

--- python/main.py
+++ python/main.py
@@ -1,5 +1,8 @@
 from car import Car
 from account import Account
+from uberX import UberX
+from user import User
+
 
 if __name__ == "__main__":
     print("Hola Mundo")
@@ -14,3 +17,14 @@ if __name__ == "__main__":
     #car2.license = "QWE567"
     #car2.driver = "Apple"
     #print(vars(car2))
+    
+    print("UberX")
+    #uberX = UberX("ADF875", Account("Felipe", "GSG458"), "Chevrolet", "Spark")    
+    #print(vars(uberX))
+    #print(vars(uberX.driver))
+    
+    print("User")
+    #user = User("Name user","123657","nameuser@gmail.com", "6753")
+    #print(vars(user))
+   
+    
